8571f3574c2ca9782b13a2762325690e146bc9dd
D4rkD0g <hallodog@hotmail.com>
2021-06-25 17:35:44 +0800

    Update getproxy.py

--- web/getproxy.py
+++ web/getproxy.py
@@ -7,36 +7,46 @@ headers = {
   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.35',
 }
 
-xiladaili = "http://www.xiladaili.com/https/"
-proxylist = []
+xiladaili = "http://www.xiladaili.com/"
 
 
-f = open("ips.txt", "w+")
+class ProxyFetcher(object):
+	"""docstring for ProxyFetcher"""
+	def __init__(self):
+		super(ProxyFetcher, self).__init__()
+		self.proxylist = set()
 
-for page in range(2, 11):
-	url = xiladaili + "{}".format(page)
+	def get_xila(self, htype="http"):
+		for page in range(2, 3):
+			url = xiladaili + "{}/{}".format(htype, page)
 
-	r = requests.get(url, headers=headers, timeout=5) 
-	if r.status_code != 200:
-		print(url, r.status_code)
-		print("Connect Error")
-		exit(-1)
+			r = requests.get(url, headers=headers, timeout=5) 
+			if r.status_code != 200:
+				print(url, r.status_code)
+				print("Connect Error")
+				exit(-1)
 
-	soup=BeautifulSoup(r.text, features="lxml")
+			soup=BeautifulSoup(r.text, features="lxml")
 
-	ips = soup.find_all(name='table',attrs={"class":"fl-table"})
-	if len(ips) == 0:
-		print("No ips")
-		continue
-		
-	for tr in ips[0].findAll("tr"):
-		if tr.td == None:
-			continue
-		ip = tr.td.text
-		print(ip)
-		f.write(ip + "\n")
-	time.sleep(random.randint(1, 4))
+			ips = soup.find_all(name='table',attrs={"class":"fl-table"})
+			if len(ips) == 0:
+				print("No ips")
+				continue
+				
+			for tr in ips[0].findAll("tr"):
+				if tr.td == None:
+					continue
+				ip = tr.td.text
+				self.proxylist.add(ip)
+			time.sleep(random.randint(1, 4))
 
-f.close()
+	def write_file(self):
+		with open("ips.txt", "w+") as f:
+			for ip in self.proxylist:
+				f.write(ip + "\n")
 
 
+if __name__ == '__main__':
+	pf = ProxyFetcher()
+	pf.get_xila(htype="https")
+	pf.write_file()
